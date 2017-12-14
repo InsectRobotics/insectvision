Fix rotation of the sky to work on 3D - fix bugs in sensor lenses orientation - update compound eyes and sensor to tilt correctly - finalise tilting and star creating datasets for tilting

compoundeye/create_sensor_dataset.py
@@ -11,7 +11,7 @@ __datadir__ = __dir__ + "../data/datasets/"
 if __name__ == "__main__":
 
     # parameters
-    nb_lenses = 100
+    nb_lenses = 60
     fov = 60
     observer = get_seville_observer()
     nb_months = 7
@@ -31,31 +31,37 @@ if __name__ == "__main__":
     months = (np.arange(start_month-1, start_month + nb_months - 1, 1) % 12) + 1
     for month in months:
         observer.date = datetime(year=2017, month=month, day=start_day, hour=0, minute=0, second=0)
-        sky = SkyModel(observer=observer, nside=1)
+        sky = SkyModel(observer=observer)
         rising = observer.next_rising(sky.sun).datetime() + timedelta(hours=1)
         setting = observer.next_setting(sky.sun).datetime() - timedelta(hours=1)
 
         observer.date = rising
         while observer.date.datetime() < setting:
-            print "Date:", observer.date,
-            sky = SkyModel(observer=observer, nside=1)
-            sky.generate()
+            sensor.sky = SkyModel(observer=observer)
 
-            r = sensor.facing_direction
-            for i in xrange(360):
-                sensor.rotate(np.deg2rad(1))
-                sensor.set_sky(sky)
-                lon = (sky.lon + sensor.facing_direction) % (2 * np.pi)
-                lat = sky.lat
+            for k in xrange(9):
+                for j in xrange(9):
+                    print "Date:", observer.date,
+                    print "Roll:", np.rad2deg(sensor.yaw_pitch_roll[2]),
+                    print "Pitch:", np.rad2deg(sensor.yaw_pitch_roll[1]),
+                    for i in xrange(360):
+                        lon = (sky.lon + sensor.yaw_pitch_roll[0]) % (2 * np.pi)
+                        lat = sky.lat
 
-                x = np.vstack([x, sensor.L.flatten()])
-                t = np.vstack([t, encode_sun(lon, lat)])
-                m = np.vstack([m, observer.date.datetime()])
-                if i % 10 == 0:
-                    print '.',
-            print " ", x.shape, t.shape
+                        x = np.vstack([x, sensor.L.flatten()])
+                        t = np.vstack([t, encode_sun(lon, lat)])
+                        m = np.vstack([m, observer.date.datetime()])
+                        if i % 10 == 0:
+                            print '.',
+                            # print '  ' * (j+1) ** 2,
+                        sensor.rotate(yaw=np.deg2rad(1))
+                    print " ", x.shape, t.shape
+                    sensor.rotate(yaw=-sensor.yaw_pitch_roll[0])
+                    sensor.rotate(pitch=np.deg2rad(10))
+                sensor.rotate(pitch=-sensor.yaw_pitch_roll[1])
+                sensor.rotate(roll=np.deg2rad(10))
+            sensor.rotate(roll=-sensor.yaw_pitch_roll[2])
 
-            sensor.facing_direction = r
             observer.date = observer.date.datetime() + delta
 
     np.savez_compressed(__datadir__ + "%s.npz" % name, x=x, t=t)

compoundeye/model.py
@@ -11,20 +11,24 @@ class CompoundEye(object):
     def __init__(self, ommatidia, central_microvili=(np.pi/6, np.pi/18), noise_factor=.1,
                  activate_dop_sensitivity=False):
 
+        # the eye facing direction
+        self.yaw_pitch_roll = np.zeros(3)
+
         # eye specifications (ommatidia topography)
-        self.theta = ommatidia[:, 0]
-        self.phi = ommatidia[:, 1]
+        self._sky = SkyModel()
+        self.theta_global = ommatidia[:, 0]
+        self.phi_global = ommatidia[:, 1]
         if ommatidia.shape[1] > 3:
             self._dop_filter = ommatidia[:, 3]
             self._aop_filter = ommatidia[:, 2]
         elif ommatidia.shape[1] > 2:
             self._aop_filter = ommatidia[:, 2]
             _, self._dop_filter = get_microvilli_angle(
-                self.theta, self.phi, theta=central_microvili[0], phi=central_microvili[1], n=noise_factor
+                self.theta_global, self.phi_global, theta=central_microvili[0], phi=central_microvili[1], n=noise_factor
             )
         else:
             self._aop_filter, self._dop_filter = get_microvilli_angle(
-                self.theta, self.phi, theta=central_microvili[0], phi=central_microvili[1], n=noise_factor
+                self.theta_global, self.phi_global, theta=central_microvili[0], phi=central_microvili[1], n=noise_factor
             )
         if not activate_dop_sensitivity:
             self._dop_filter[:] = 1.
@@ -32,13 +36,6 @@ class CompoundEye(object):
 
         self._channel_filters = {}
         self._update_filters()
-
-        # the raw receptive information
-        self._lum = np.zeros_like(self.theta)
-        self._dop = np.ones_like(self._dop_filter)
-        self._aop = np.zeros_like(self._aop_filter)
-
-        self.facing_direction = 0
 
     def activate_pol_filters(self, value):
         """
@@ -51,6 +48,14 @@ class CompoundEye(object):
         self._active_pol_filters = value
         if value != old_value:
             self._update_filters()
+
+    @property
+    def sky(self):
+        return self._sky
+
+    @sky.setter
+    def sky(self, value):
+        self._sky = value
 
     @property
     def dop_filter(self):
@@ -66,26 +71,69 @@ class CompoundEye(object):
         for kk in ['r', 'g', 'b', 'uv']:
             if kk in ks:
                 k.append(kk)
+        i, d, a = self.sky.L / np.sqrt(2), self.sky.DOP, self.sky.AOP
         lum_channels = np.array([
-            pf(cf(self._lum), self._aop, self._dop) for cf, pf in [self._channel_filters[c] for c in k]
+            pf(cf(i), a, d) for cf, pf in [self._channel_filters[c] for c in k]
         ]).T
         return np.clip(lum_channels, 0, 1)
 
     @property
     def DOP(self):
-        return self._dop
+        return self.sky.DOP
 
     @property
     def AOP(self):
-        return self._aop
+        return self.sky.AOP
 
-    def set_sky(self, sky):
-        self._lum, self._dop, self._aop = sky.get_features(np.pi/2-self.theta, np.pi-self.phi+self.facing_direction)
-        self._lum /= np.sqrt(2)
-        self._dop[np.isnan(self._dop)] = 1.
+    @property
+    def theta_global(self):
+        return (np.pi/2 - self.sky.theta_z + np.pi) % (2 * np.pi) - np.pi  # type: np.ndarray
 
-    def rotate(self, angle):
-        self.facing_direction -= angle
+    @theta_global.setter
+    def theta_global(self, value):
+        """
+        :param value: the ommatidia elevation
+        :type value: np.ndarray
+        :return:
+        """
+        self.sky.theta_z = (np.pi/2 - value + np.pi) % (2 * np.pi) - np.pi
+
+    @property
+    def phi_global(self):
+        return (2 * np.pi - self.sky.phi_z + self.yaw_pitch_roll[0]) % (2 * np.pi) - np.pi
+
+    @phi_global.setter
+    def phi_global(self, value):
+        self.sky.phi_z = (2 * np.pi - value + self.yaw_pitch_roll[0]) % (2 * np.pi) - np.pi
+
+    @property
+    def theta_local(self):
+        theta_z, phi_z = SkyModel.rotate(self.sky.theta_z, self.sky.phi_z, yaw=-self.yaw_pitch_roll[0])
+        theta_z, phi_z = SkyModel.rotate(theta_z, phi_z, pitch=-self.yaw_pitch_roll[1])
+        theta_z, phi_z = SkyModel.rotate(theta_z, phi_z, roll=-self.yaw_pitch_roll[2])
+        return (np.pi / 2 - theta_z + np.pi) % (2 * np.pi) - np.pi
+
+    @property
+    def phi_local(self):
+        theta_z, phi_z = SkyModel.rotate(self.sky.theta_z, self.sky.phi_z, yaw=-self.yaw_pitch_roll[0])
+        theta_z, phi_z = SkyModel.rotate(theta_z, phi_z, pitch=-self.yaw_pitch_roll[1])
+        theta_z, phi_z = SkyModel.rotate(theta_z, phi_z, roll=-self.yaw_pitch_roll[2])
+        return (2 * np.pi - phi_z) % (2 * np.pi) - np.pi
+
+    def rotate(self, yaw=0., pitch=0., roll=0.):
+        # rotate back to the default orientation
+        sky = SkyModel.rotate_sky(self.sky, yaw=-self.yaw_pitch_roll[0])
+        sky = SkyModel.rotate_sky(sky, pitch=-self.yaw_pitch_roll[1])
+        sky = SkyModel.rotate_sky(sky, roll=-self.yaw_pitch_roll[2])
+
+        # update the facing direction of the eye
+        self.yaw_pitch_roll = self.rotate_centre(self.yaw_pitch_roll, yaw=yaw, pitch=pitch, roll=roll)
+
+        # rotate the sky according to the new facing direction
+        self.sky = SkyModel.rotate_sky(sky,
+                                       yaw=self.yaw_pitch_roll[0],
+                                       pitch=self.yaw_pitch_roll[1],
+                                       roll=self.yaw_pitch_roll[2])
 
     def _update_filters(self):
 
@@ -103,6 +151,16 @@ class CompoundEye(object):
                 POLFilter(self._aop_filter, self.dop_filter, name="UVPOLFilter")
             ]
         }
+
+    @staticmethod
+    def rotate_centre(centre, yaw=0., pitch=0., roll=0.):
+        centre[[1, 0]] = SkyModel.rotate(np.pi / 2 - centre[1], np.pi - centre[0], yaw=yaw, pitch=pitch)
+
+        centre[0] = (2 * np.pi - centre[0]) % (2 * np.pi) - np.pi
+        centre[1] = (3 * np.pi/2 - centre[1]) % (2 * np.pi) - np.pi
+        centre[2] = (centre[2] + roll + np.pi) % (2 * np.pi) - np.pi
+
+        return centre
 
 
 class Filter(object):
@@ -195,32 +253,34 @@ if __name__ == "__main__":
     from beeeye import load_both_eyes
     import matplotlib.pyplot as plt
 
-    angle = 0
+    angle = [np.pi/2, 0., np.pi/3]
 
     # initialise sky
     obs = city("Edinburgh")
     obs.date = datetime.now()
-    sky = SkyModel(observer=obs, nside=1)
-    sky.generate()
 
     # initialise ommatidia features
     ommatidia_left, ommatidia_right = load_both_eyes()
     l_eye = CompoundEye(ommatidia_left)
-    l_eye.rotate(angle)
+    l_eye.rotate(*angle)
     r_eye = CompoundEye(ommatidia_right)
-    r_eye.rotate(angle)
-    l_eye.set_sky(sky)
-    r_eye.set_sky(sky)
+    r_eye.rotate(*angle)
+    l_eye.sky.obs = obs
+    r_eye.sky.obs = obs
 
     # plot result
     s, p = 20, 4
     # plot eye's structure
+    norm = lambda x: x / 25. / np.sqrt(2)
+
+    r_theta, r_phi = r_eye.theta_local, r_eye.phi_local
+    l_theta, l_phi = l_eye.theta_local, l_eye.phi_local
     if True:
         plt.figure("Compound eyes - Structure", figsize=(8, 21))
 
-        lum_r = l_eye._lum + (1. - l_eye._lum) * .05
-        lum_g = l_eye._lum + (1. - l_eye._lum) * .53
-        lum_b = l_eye._lum + (1. - l_eye._lum) * .79
+        lum_r = norm(l_eye.sky.L) + (1. - norm(l_eye.sky.L)) * .05
+        lum_g = norm(l_eye.sky.L) + (1. - norm(l_eye.sky.L)) * .53
+        lum_b = norm(l_eye.sky.L) + (1. - norm(l_eye.sky.L)) * .79
         L = np.clip(np.concatenate((
             lum_r[..., np.newaxis],
             lum_g[..., np.newaxis],
@@ -228,7 +288,8 @@ if __name__ == "__main__":
         ), axis=-1), 0, 1)
         plt.subplot(321)
         plt.title("Left")
-        plt.scatter(l_eye.phi, l_eye.theta, c=L, marker=".", s=np.power(s, p * np.absolute(l_eye.theta) / np.pi))
+        print np.rad2deg(l_eye.yaw_pitch_roll)
+        plt.scatter(l_phi, l_theta, c=L, marker=".", s=np.power(s, p * np.absolute(l_theta) / np.pi))
         plt.ylabel("epsilon (elevation)")
         plt.xlim([-np.pi, np.pi])
         plt.ylim([-np.pi/2, np.pi/2])
@@ -236,9 +297,9 @@ if __name__ == "__main__":
         plt.yticks([-np.pi/2, -np.pi/3, -np.pi/6, 0, np.pi/6, np.pi/3, np.pi/2],
                    ["-90", "-60", "-30", "0", "30", "60", "90"])
 
-        lum_r = r_eye._lum + (1. - r_eye._lum) * .05
-        lum_g = r_eye._lum + (1. - r_eye._lum) * .53
-        lum_b = r_eye._lum + (1. - r_eye._lum) * .79
+        lum_r = norm(r_eye.sky.L) + (1. - norm(r_eye.sky.L)) * .05
+        lum_g = norm(r_eye.sky.L) + (1. - norm(r_eye.sky.L)) * .53
+        lum_b = norm(r_eye.sky.L) + (1. - norm(r_eye.sky.L)) * .79
         L = np.clip(np.concatenate((
             lum_r[..., np.newaxis],
             lum_g[..., np.newaxis],
@@ -246,15 +307,15 @@ if __name__ == "__main__":
         ), axis=-1), 0, 1)
         plt.subplot(322)
         plt.title("Right")
-        plt.scatter(r_eye.phi, r_eye.theta, c=L, marker=".", s=np.power(s, p * np.absolute(l_eye.theta) / np.pi))
+        plt.scatter(r_phi, r_theta, c=L, marker=".", s=np.power(s, p * np.absolute(r_theta) / np.pi))
         plt.xlim([-np.pi, np.pi])
         plt.ylim([-np.pi/2, np.pi/2])
         plt.xticks([-np.pi, -3*np.pi/4, -np.pi/2, -np.pi/4, 0, np.pi/4, np.pi/2, 3*np.pi/4, np.pi], [])
         plt.yticks([-np.pi/2, -np.pi/3, -np.pi/6, 0, np.pi/6, np.pi/3, np.pi/2], [])
 
         plt.subplot(323)
-        plt.scatter(l_eye.phi, l_eye.theta, c=l_eye._dop_filter, cmap="Blues", vmin=0, vmax=1,
-                    marker=".", s=np.power(s, p * np.absolute(l_eye.theta) / np.pi))
+        plt.scatter(l_phi, l_theta, c=l_eye._dop_filter, cmap="Blues", vmin=0, vmax=1,
+                    marker=".", s=np.power(s, p * np.absolute(l_theta) / np.pi))
         plt.ylabel("epsilon (elevation)")
         plt.xlim([-np.pi, np.pi])
         plt.ylim([-np.pi / 2, np.pi / 2])
@@ -263,16 +324,16 @@ if __name__ == "__main__":
                    ["-90", "-60", "-30", "0", "30", "60", "90"])
 
         plt.subplot(324)
-        plt.scatter(r_eye.phi, r_eye.theta, c=r_eye._dop_filter, cmap="Blues", vmin=0, vmax=1,
-                    marker=".", s=np.power(s, p * np.absolute(l_eye.theta) / np.pi))
+        plt.scatter(r_phi, r_theta, c=r_eye._dop_filter, cmap="Blues", vmin=0, vmax=1,
+                    marker=".", s=np.power(s, p * np.absolute(r_theta) / np.pi))
         plt.xlim([-np.pi, np.pi])
         plt.ylim([-np.pi / 2, np.pi / 2])
         plt.xticks([-np.pi, -3 * np.pi / 4, -np.pi / 2, -np.pi / 4, 0, np.pi / 4, np.pi / 2, 3 * np.pi / 4, np.pi], [])
         plt.yticks([-np.pi / 2, -np.pi / 3, -np.pi / 6, 0, np.pi / 6, np.pi / 3, np.pi / 2], [])
 
         plt.subplot(325)
-        plt.scatter(l_eye.phi, l_eye.theta, c=l_eye._aop_filter % np.pi, cmap="hsv", vmin=0, vmax=np.pi,
-                    marker=".", s=np.power(s, p * np.absolute(l_eye.theta) / np.pi))
+        plt.scatter(l_phi, l_theta, c=l_eye._aop_filter % np.pi, cmap="hsv", vmin=0, vmax=np.pi,
+                    marker=".", s=np.power(s, p * np.absolute(l_theta) / np.pi))
         plt.xlabel("alpha (azimuth)")
         plt.ylabel("epsilon (elevation)")
         plt.xlim([-np.pi, np.pi])
@@ -283,8 +344,8 @@ if __name__ == "__main__":
                    ["-90", "-60", "-30", "0", "30", "60", "90"])
 
         plt.subplot(326)
-        plt.scatter(r_eye.phi, r_eye.theta, c=r_eye._aop_filter % np.pi, cmap="hsv", vmin=0, vmax=np.pi,
-                    marker=".", s=np.power(s, p * np.absolute(l_eye.theta) / np.pi))
+        plt.scatter(r_phi, r_theta, c=r_eye._aop_filter % np.pi, cmap="hsv", vmin=0, vmax=np.pi,
+                    marker=".", s=np.power(s, p * np.absolute(r_theta) / np.pi))
         plt.xlabel("alpha (azimuth)")
         plt.xlim([-np.pi, np.pi])
         plt.ylim([-np.pi / 2, np.pi / 2])
@@ -297,7 +358,7 @@ if __name__ == "__main__":
 
         plt.subplot(321)
         plt.title("Left")
-        plt.scatter(l_eye.phi, l_eye.theta, c=l_eye.L, marker=".", s=np.power(s, p * np.absolute(l_eye.theta) / np.pi))
+        plt.scatter(l_phi, l_theta, c=l_eye.L, marker=".", s=np.power(s, p * np.absolute(l_theta) / np.pi))
         plt.ylabel("epsilon (elevation)")
         plt.xlim([-np.pi, np.pi])
         plt.ylim([-np.pi/2, np.pi/2])
@@ -307,15 +368,15 @@ if __name__ == "__main__":
 
         plt.subplot(322)
         plt.title("Right")
-        plt.scatter(r_eye.phi, r_eye.theta, c=r_eye.L, marker=".", s=np.power(s, p * np.absolute(l_eye.theta) / np.pi))
+        plt.scatter(r_phi, r_theta, c=r_eye.L, marker=".", s=np.power(s, p * np.absolute(r_theta) / np.pi))
         plt.xlim([-np.pi, np.pi])
         plt.ylim([-np.pi/2, np.pi/2])
         plt.xticks([-np.pi, -3*np.pi/4, -np.pi/2, -np.pi/4, 0, np.pi/4, np.pi/2, 3*np.pi/4, np.pi], [])
         plt.yticks([-np.pi/2, -np.pi/3, -np.pi/6, 0, np.pi/6, np.pi/3, np.pi/2], [])
 
         plt.subplot(323)
-        plt.scatter(l_eye.phi, l_eye.theta, c=l_eye.DOP, cmap="Blues", vmin=0, vmax=1,
-                    marker=".", s=np.power(s, p * np.absolute(l_eye.theta) / np.pi))
+        plt.scatter(l_phi, l_theta, c=l_eye.DOP, cmap="Blues", vmin=0, vmax=1,
+                    marker=".", s=np.power(s, p * np.absolute(l_theta) / np.pi))
         plt.ylabel("epsilon (elevation)")
         plt.xlim([-np.pi, np.pi])
         plt.ylim([-np.pi / 2, np.pi / 2])
@@ -324,16 +385,16 @@ if __name__ == "__main__":
                    ["-90", "-60", "-30", "0", "30", "60", "90"])
 
         plt.subplot(324)
-        plt.scatter(r_eye.phi, r_eye.theta, c=r_eye.DOP, cmap="Blues", vmin=0, vmax=1,
-                    marker=".", s=np.power(s, p * np.absolute(l_eye.theta) / np.pi))
+        plt.scatter(r_phi, r_theta, c=r_eye.DOP, cmap="Blues", vmin=0, vmax=1,
+                    marker=".", s=np.power(s, p * np.absolute(r_theta) / np.pi))
         plt.xlim([-np.pi, np.pi])
         plt.ylim([-np.pi / 2, np.pi / 2])
         plt.xticks([-np.pi, -3 * np.pi / 4, -np.pi / 2, -np.pi / 4, 0, np.pi / 4, np.pi / 2, 3 * np.pi / 4, np.pi], [])
         plt.yticks([-np.pi / 2, -np.pi / 3, -np.pi / 6, 0, np.pi / 6, np.pi / 3, np.pi / 2], [])
 
         plt.subplot(325)
-        plt.scatter(l_eye.phi, l_eye.theta, c=l_eye.AOP, cmap="hsv", vmin=0, vmax=np.pi,
-                    marker=".", s=np.power(s, p * np.absolute(l_eye.theta) / np.pi))
+        plt.scatter(l_phi, l_theta, c=l_eye.AOP, cmap="hsv", vmin=0, vmax=np.pi,
+                    marker=".", s=np.power(s, p * np.absolute(l_theta) / np.pi))
         plt.xlabel("alpha (azimuth)")
         plt.ylabel("epsilon (elevation)")
         plt.xlim([-np.pi, np.pi])
@@ -344,8 +405,8 @@ if __name__ == "__main__":
                    ["-90", "-60", "-30", "0", "30", "60", "90"])
 
         plt.subplot(326)
-        plt.scatter(r_eye.phi, r_eye.theta, c=r_eye.AOP, cmap="hsv", vmin=0, vmax=np.pi,
-                    marker=".", s=np.power(s, p * np.absolute(l_eye.theta) / np.pi))
+        plt.scatter(r_phi, r_theta, c=r_eye.AOP, cmap="hsv", vmin=0, vmax=np.pi,
+                    marker=".", s=np.power(s, p * np.absolute(r_theta) / np.pi))
         plt.xlabel("alpha (azimuth)")
         plt.xlim([-np.pi, np.pi])
         plt.ylim([-np.pi / 2, np.pi / 2])

compoundeye/sensor.py
@@ -59,6 +59,8 @@ class CompassSensor(CompoundEye):
             fit = False
         if not fit or nb_lenses > 100:
             thetas, phis = fibonacci_sphere(nb_lenses, np.rad2deg(fov))
+        thetas = (3 * np.pi/2 - thetas) % (2 * np.pi) - np.pi
+        phis = (2 * np.pi - phis) % (2 * np.pi) - np.pi
 
         super(CompassSensor, self).__init__(
             ommatidia=np.array([thetas.flatten(), phis.flatten()]).T,
@@ -111,8 +113,7 @@ class CompassSensor(CompoundEye):
                 x = self.__x_last
         return x
 
-    def set_sky(self, sky):
-        super(CompassSensor, self).set_sky(sky)
+    def refresh(self):
         if not np.all(self.__x_new == 0.):
             self.__x_last = self.__x_new
         self.__x_new = super(CompassSensor, self).L.flatten()
@@ -130,16 +131,16 @@ class CompassSensor(CompoundEye):
             sky_model = x  # type: ChromaticitySkyModel
             x = np.empty((0, self.L.size), dtype=np.float32)
             t = np.empty((0, NB_EN), dtype=np.float32)
-            r = self.facing_direction
+            r = self.yaw_pitch_roll[0]
             for j in xrange(180):
                 self.rotate(np.deg2rad(2))
-                self.set_sky(sky_model)
-                lon = (sky_model.lon + self.facing_direction) % (2 * np.pi)
+                self.sky = sky_model
+                lon = (sky_model.lon + self.yaw_pitch_roll[0]) % (2 * np.pi)
                 lat = sky_model.lat
                 x = np.vstack([x, self.L.flatten()])
                 t = np.vstack([t, encode_sun(lon, lat)])
-            self.facing_direction = r
-            self.set_sky(sky_model)
+            self.yaw_pitch_roll[0] = r
+            self.sky = sky_model
 
         # self.w = (1. - self.learning_rate) * self.w + self.learning_rate * la.pinv(x).dot(t)
         self.w_whitening = pca_kernel(x)
@@ -153,7 +154,7 @@ class CompassSensor(CompoundEye):
         if isinstance(args[0], np.ndarray):
             self._lum = args[0]  # type: np.ndarray
         elif isinstance(args[0], SkyModel):
-            self.set_sky(args[0])
+            self.sky = args[0]
         else:
             raise AttributeError("Unknown attribute type: %s" % type(args[0]))
         decode = False
@@ -199,7 +200,7 @@ class CompassSensor(CompoundEye):
         import matplotlib.pyplot as plt
         from matplotlib.patches import Ellipse, Rectangle
 
-        xyz = sph2vec(np.pi/2 - sensor.theta, sensor.phi, sensor.R_c)
+        xyz = sph2vec(sensor.theta_local, sensor.phi_local, sensor.R_c)
 
         plt.figure("Sensor Design", figsize=(10, 10))
 
@@ -220,7 +221,7 @@ class CompassSensor(CompoundEye):
         sensor_outline.set_alpha(.5)
         sensor_outline.set_facecolor("grey")
 
-        stheta, sphi, sL = sensor.theta, sensor.phi, sensor.L
+        stheta, sphi, sL = np.pi/2 - sensor.theta_local, sensor.phi_local, sensor.L
         if sensor.mode == "event":
             sL = np.clip(1. * sL + .5, 0., 1.)
         for (x, y, z), th, ph, L in zip(xyz.T, stheta, sphi, sL):
@@ -368,30 +369,70 @@ def decode_sun(x):
     return lon, lat
 
 
+if __name__ == "__main__1__":
+    import matplotlib.pyplot as plt
+    from sky import get_seville_observer
+    from datetime import datetime
+
+    s, p = 20, 4
+    # modes: "normal", "cross", "event"
+    sensor = CompassSensor(nb_lenses=1000, fov=np.deg2rad(90), mode="normal")
+    # s.load_weights()
+
+    # default observer is in Seville (where the data come from)
+    observer = get_seville_observer()
+    observer.date = datetime(2018, 6, 21, 10, 0, 0)
+
+    sensor.sky.obs = observer
+    for angle in np.linspace(0, np.pi/2, 9):
+        plt.figure("Sensor - values", figsize=(8, 21))
+        sensor.rotate(pitch=np.pi / 18)
+        sensor.refresh()
+
+        lum_r = sensor.L * .05
+        lum_g = sensor.L * .53
+        lum_b = sensor.L * .79
+        L = np.clip(np.concatenate((
+            lum_r[..., np.newaxis],
+            lum_g[..., np.newaxis],
+            lum_b[..., np.newaxis]
+        ), axis=-1), 0, 1)
+        print np.rad2deg(sensor.yaw_pitch_roll)
+        plt.scatter(sensor.phi_local, sensor.theta_local,
+                    c=L, marker=".", s=np.power(s, p * np.absolute(sensor.theta_local) / np.pi))
+        plt.ylabel("epsilon (elevation)")
+        plt.xlim([-np.pi, np.pi])
+        plt.ylim([-np.pi / 2, np.pi / 2])
+        plt.xticks([-np.pi, -3 * np.pi / 4, -np.pi / 2, -np.pi / 4, 0, np.pi / 4, np.pi / 2, 3 * np.pi / 4, np.pi], [])
+        plt.yticks([-np.pi / 2, -np.pi / 3, -np.pi / 6, 0, np.pi / 6, np.pi / 3, np.pi / 2],
+                   ["-90", "-60", "-30", "0", "30", "60", "90"])
+
+        plt.show()
+
+
 if __name__ == "__main__":
     from sky import get_seville_observer
     from datetime import datetime
 
     # modes: "normal", "cross", "event"
-    s = CompassSensor(nb_lenses=100, fov=np.deg2rad(60), mode="normal")
+    s = CompassSensor(nb_lenses=1000, fov=np.deg2rad(60), mode="normal")
     # s.load_weights()
 
     # default observer is in Seville (where the data come from)
     observer = get_seville_observer()
-    observer.date = datetime.now()
+    observer.date = datetime(2018, 6, 21, 10, 0, 0)
 
-    # create and generate a sky instance
-    sky = SkyModel(observer=observer, nside=1)
-    sky.generate()
-
-    # lon, lat = sky.lon, sky.lat
-    # print "Reality: Lon = %.2f, Lat = %.2f" % (np.rad2deg(lon), np.rad2deg(lat))
-    # lon, lat = s.update_parameters(sky)
-    # print "Prediction: Lon = %.2f, Lat = %.2f" % (np.rad2deg(lon), np.rad2deg(lat))
-    s.set_sky(sky)
-    # s.rotate(np.deg2rad(90))
-    # s.set_sky(sky)
-    CompassSensor.visualise(s)
+    s.sky.obs = observer
+    for angle in np.linspace(0, np.pi/2, 9):
+        s.rotate(yaw=np.pi/6, roll=np.pi / 18)
+        s.refresh()
+        # lon, lat = sky.lon, sky.lat
+        # print "Reality: Lon = %.2f, Lat = %.2f" % (np.rad2deg(lon), np.rad2deg(lat))
+        # lon, lat = s.update_parameters(sky)
+        # print "Prediction: Lon = %.2f, Lat = %.2f" % (np.rad2deg(lon), np.rad2deg(lat))
+        # s.rotate(np.deg2rad(90))
+        # s.set_sky(sky)
+        CompassSensor.visualise(s)
 
 
 if __name__ == "__main__2__":
@@ -408,10 +449,10 @@ if __name__ == "__main__2__":
     observer.date = datetime.now()
 
     # create and generate a sky instance
-    sky = ChromaticitySkyModel(observer=observer, nside=1)
+    sky = SkyModel(observer=observer, nside=1)
     sky.generate()
 
-    s.set_sky(sky)
+    s.sky = sky
     p[i] = s.L
     # p[i] = s.DOP
     # p[i] = np.rad2deg(s.AOP)

sphere/transform.py
@@ -27,8 +27,8 @@ def vec2sph(vec):
     """
     Transforms a cartessian vector to spherical coordinates.
     :param vec:     the cartessian vector
-    :return theta:  elevation
-    :return phi:    azimuth
+    :return theta_z:  elevation
+    :return phi_z:    azimuth
     :return rho:    radius
     """
     rho = la.norm(vec, axis=0)  # length of the radius
@@ -81,7 +81,7 @@ def sphadj(theta=None, phi=None,
     :param phi_max:     the azimuth upper bound (default pi)
     """
 
-    # change = np.any([theta < -np.pi / 2, theta > np.pi / 2], axis=0)
+    # change = np.any([theta_z < -np.pi / 2, theta_z > np.pi / 2], axis=0)
     if theta is not None:
         if (theta >= theta_max).all():
             theta = np.pi - theta