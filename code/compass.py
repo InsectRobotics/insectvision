Fix rotation of the sky to work on 3D - fix bugs in sensor lenses orientation - update compound eyes and sensor to tilt correctly - change the whitening method of the sensor from PCA to ZCA - test that the sensor rotates properly - create new data using the fixed sensor

code/compass.py
@@ -9,14 +9,15 @@ def encode_sph(theta, phi=None, length=8):
             theta = theta[0]
         else:
             phi = theta
-            theta = 1.
+            theta = np.pi / 2
+    theta = np.absolute(theta)
     alpha = np.linspace(0, 2 * np.pi, length, endpoint=False)
     return np.sin(alpha + phi + np.pi / 2) * theta / (length / 2.)
 
 
 def decode_sph(I):
     fund_freq = np.fft.fft(I)[1]
-    phi = -np.angle(np.conj(fund_freq))
+    phi = (np.pi - np.angle(np.conj(fund_freq))) % (2 * np.pi) + np.pi
     theta = np.absolute(fund_freq)
     return np.array([theta, phi])
 

compoundeye/create_sensor_dataset.py
@@ -11,8 +11,8 @@ __datadir__ = __dir__ + "../data/datasets/"
 if __name__ == "__main__":
 
     # parameters
-    nb_lenses = 60
-    fov = 60
+    nb_lenses = 368
+    fov = 180
     observer = get_seville_observer()
     nb_months = 7
     start_month = 6
@@ -31,38 +31,37 @@ if __name__ == "__main__":
     months = (np.arange(start_month-1, start_month + nb_months - 1, 1) % 12) + 1
     for month in months:
         observer.date = datetime(year=2017, month=month, day=start_day, hour=0, minute=0, second=0)
-        sky = SkyModel(observer=observer)
-        rising = observer.next_rising(sky.sun).datetime() + timedelta(hours=1)
-        setting = observer.next_setting(sky.sun).datetime() - timedelta(hours=1)
+        sensor.sky.obs = observer
+        rising = observer.next_rising(sensor.sky.sun).datetime() + timedelta(hours=1)
+        setting = observer.next_setting(sensor.sky.sun).datetime() - timedelta(hours=1)
 
-        observer.date = rising
-        while observer.date.datetime() < setting:
-            sensor.sky = SkyModel(observer=observer)
+        sensor.sky.obs.date = rising
+        while sensor.sky.obs.date.datetime() < setting:
+            sensor.sky.generate()
 
-            for k in xrange(9):
-                for j in xrange(9):
-                    print "Date:", observer.date,
-                    print "Roll:", np.rad2deg(sensor.yaw_pitch_roll[2]),
-                    print "Pitch:", np.rad2deg(sensor.yaw_pitch_roll[1]),
-                    for i in xrange(360):
-                        lon = (sky.lon + sensor.yaw_pitch_roll[0]) % (2 * np.pi)
-                        lat = sky.lat
+            print "Date:", sensor.sky.obs.date,
+            for i in xrange(360):
+                sensor.refresh()
+                # lon, lat, _ = CompassSensor.rotate_centre(
+                #     np.array([sensor.sky.lon, sensor.sky.lat, 0]),
+                #     yaw=sensor.yaw,
+                #     pitch=sensor.pitch,
+                #     roll=sensor.roll
+                # )
+                lat = sensor.sky.lat
+                lon = (sensor.sky.lon - sensor.yaw + np.pi) % (2 * np.pi) - np.pi
+                x = np.vstack([x, sensor.L])
+                t = np.vstack([t, encode_sun(lon, lat)])
+                m = np.vstack([m, sensor.sky.obs.date.datetime()])
+                if i % 10 == 0:
+                    print '.',
+                    # print '  ' * (j+1) ** 2,
+                sensor.rotate(yaw=np.deg2rad(1))
+            sensor.refresh()
+            print " ", x.shape, t.shape
+            # sensor.rotate(yaw=-sensor.yaw)
 
-                        x = np.vstack([x, sensor.L.flatten()])
-                        t = np.vstack([t, encode_sun(lon, lat)])
-                        m = np.vstack([m, observer.date.datetime()])
-                        if i % 10 == 0:
-                            print '.',
-                            # print '  ' * (j+1) ** 2,
-                        sensor.rotate(yaw=np.deg2rad(1))
-                    print " ", x.shape, t.shape
-                    sensor.rotate(yaw=-sensor.yaw_pitch_roll[0])
-                    sensor.rotate(pitch=np.deg2rad(10))
-                sensor.rotate(pitch=-sensor.yaw_pitch_roll[1])
-                sensor.rotate(roll=np.deg2rad(10))
-            sensor.rotate(roll=-sensor.yaw_pitch_roll[2])
-
-            observer.date = observer.date.datetime() + delta
+            sensor.sky.obs.date = sensor.sky.obs.date.datetime() + delta
 
     np.savez_compressed(__datadir__ + "%s.npz" % name, x=x, t=t)
     np.savez_compressed(__datadir__ + "M%02d-D%04d.npz" % (nb_months, delta.seconds), m=m)

compoundeye/model.py
@@ -55,6 +55,8 @@ class CompoundEye(object):
 
     @sky.setter
     def sky(self, value):
+        value.theta_z = self.theta_global
+        value.phi_z = self.phi_local
         self._sky = value
 
     @property
@@ -87,7 +89,7 @@ class CompoundEye(object):
 
     @property
     def theta_global(self):
-        return (np.pi/2 - self.sky.theta_z + np.pi) % (2 * np.pi) - np.pi  # type: np.ndarray
+        return (self.sky.theta_z + np.pi) % (2 * np.pi) - np.pi  # type: np.ndarray
 
     @theta_global.setter
     def theta_global(self, value):
@@ -96,44 +98,74 @@ class CompoundEye(object):
         :type value: np.ndarray
         :return:
         """
-        self.sky.theta_z = (np.pi/2 - value + np.pi) % (2 * np.pi) - np.pi
+        self.sky.theta_z = (value + np.pi) % (2 * np.pi) - np.pi
 
     @property
     def phi_global(self):
-        return (2 * np.pi - self.sky.phi_z + self.yaw_pitch_roll[0]) % (2 * np.pi) - np.pi
+        return (self.sky.phi_z + np.pi) % (2 * np.pi) - np.pi
 
     @phi_global.setter
     def phi_global(self, value):
-        self.sky.phi_z = (2 * np.pi - value + self.yaw_pitch_roll[0]) % (2 * np.pi) - np.pi
+        self.sky.phi_z = (value + np.pi) % (2 * np.pi) - np.pi
 
     @property
     def theta_local(self):
-        theta_z, phi_z = SkyModel.rotate(self.sky.theta_z, self.sky.phi_z, yaw=-self.yaw_pitch_roll[0])
-        theta_z, phi_z = SkyModel.rotate(theta_z, phi_z, pitch=-self.yaw_pitch_roll[1])
-        theta_z, phi_z = SkyModel.rotate(theta_z, phi_z, roll=-self.yaw_pitch_roll[2])
-        return (np.pi / 2 - theta_z + np.pi) % (2 * np.pi) - np.pi
+        theta_z, phi_z = SkyModel.rotate(self.theta_global, self.phi_global, yaw=-self.yaw)
+        theta_z, phi_z = SkyModel.rotate(theta_z, phi_z, pitch=-self.pitch)
+        theta_z, phi_z = SkyModel.rotate(theta_z, phi_z, roll=-self.roll)
+        return (theta_z + np.pi) % (2 * np.pi) - np.pi
 
     @property
     def phi_local(self):
-        theta_z, phi_z = SkyModel.rotate(self.sky.theta_z, self.sky.phi_z, yaw=-self.yaw_pitch_roll[0])
-        theta_z, phi_z = SkyModel.rotate(theta_z, phi_z, pitch=-self.yaw_pitch_roll[1])
-        theta_z, phi_z = SkyModel.rotate(theta_z, phi_z, roll=-self.yaw_pitch_roll[2])
-        return (2 * np.pi - phi_z) % (2 * np.pi) - np.pi
+        theta_z, phi_z = SkyModel.rotate(self.theta_global, self.phi_global, yaw=-self.yaw)
+        theta_z, phi_z = SkyModel.rotate(theta_z, phi_z, pitch=-self.pitch)
+        theta_z, phi_z = SkyModel.rotate(theta_z, phi_z, roll=-self.roll)
+        return (np.pi + phi_z) % (2 * np.pi) - np.pi
+
+    @property
+    def yaw(self):
+        return self.yaw_pitch_roll[0]
+
+    @yaw.setter
+    def yaw(self, value):
+        self.yaw_pitch_roll[0] = value
+
+    @property
+    def pitch(self):
+        return self.yaw_pitch_roll[1]
+
+    @pitch.setter
+    def pitch(self, value):
+        self.yaw_pitch_roll[1] = value
+
+    @property
+    def roll(self):
+        return self.yaw_pitch_roll[2]
+
+    @roll.setter
+    def roll(self, value):
+        self.yaw_pitch_roll[2] = value
 
     def rotate(self, yaw=0., pitch=0., roll=0.):
+        sky = self.sky
+
         # rotate back to the default orientation
-        sky = SkyModel.rotate_sky(self.sky, yaw=-self.yaw_pitch_roll[0])
-        sky = SkyModel.rotate_sky(sky, pitch=-self.yaw_pitch_roll[1])
-        sky = SkyModel.rotate_sky(sky, roll=-self.yaw_pitch_roll[2])
+        sky = SkyModel.rotate_sky(sky, yaw=-self.yaw)
+        sky = SkyModel.rotate_sky(sky, pitch=-self.pitch)
+        sky = SkyModel.rotate_sky(sky, roll=-self.roll)
 
         # update the facing direction of the eye
-        self.yaw_pitch_roll = self.rotate_centre(self.yaw_pitch_roll, yaw=yaw, pitch=pitch, roll=roll)
+        self.yaw_pitch_roll = self.rotate_centre(
+            self.yaw_pitch_roll, yaw=yaw, pitch=pitch, roll=roll
+        )
 
         # rotate the sky according to the new facing direction
-        self.sky = SkyModel.rotate_sky(sky,
-                                       yaw=self.yaw_pitch_roll[0],
-                                       pitch=self.yaw_pitch_roll[1],
-                                       roll=self.yaw_pitch_roll[2])
+        sky = SkyModel.rotate_sky(
+            sky, yaw=self.yaw, pitch=self.pitch, roll=self.roll
+        )
+
+        self.sky.theta_z = sky.theta_z
+        self.sky.phi_z = sky.phi_z
 
     def _update_filters(self):
 
@@ -154,6 +186,7 @@ class CompoundEye(object):
 
     @staticmethod
     def rotate_centre(centre, yaw=0., pitch=0., roll=0.):
+        # centre[[1, 0]] = SkyModel.rotate(centre[1], centre[0], yaw=yaw, pitch=pitch, roll=roll)
         centre[[1, 0]] = SkyModel.rotate(np.pi / 2 - centre[1], np.pi - centre[0], yaw=yaw, pitch=pitch)
 
         centre[0] = (2 * np.pi - centre[0]) % (2 * np.pi) - np.pi

compoundeye/sensor.py
@@ -5,7 +5,7 @@ import numpy as np
 import numpy.linalg as la
 
 from geometry import fibonacci_sphere, angles_distribution, LENS_RADIUS, A_lens
-from learn.whitening import pca as pca_kernel
+from learn.whitening import zca
 from model import CompoundEye
 from sky import SkyModel
 from code.compass import encode_sph, decode_sph
@@ -20,7 +20,7 @@ DEBUG = False
 
 class CompassSensor(CompoundEye):
 
-    def __init__(self, nb_lenses=20, fov=np.deg2rad(60), mode="normal"):
+    def __init__(self, nb_lenses=20, fov=np.deg2rad(60), kernel=zca, mode="normal"):
 
         self.nb_lenses = nb_lenses
         self.fov = fov
@@ -35,6 +35,7 @@ class CompassSensor(CompoundEye):
         self.alpha = self.R_c * np.sin(fov / 2)
         self.height = self.R_c * (1. - np.cos(fov / 2))
         self.learning_rate = 0.1
+        self.kernel = kernel
 
         assert mode in ["normal", "cross", "event"],\
             "Mode has to be one of 'normal', 'cross' or 'event'."
@@ -100,9 +101,13 @@ class CompassSensor(CompoundEye):
         else:
             x = self.__x_new
 
+        x[np.isnan(x)] = 0.
         x_max = x.max()
         x_min = x.min()
-        x = (x - x_min) / (x_max - x_min)
+        if (x_max - x_min) > 0:
+            x = (x - x_min) / (x_max - x_min)
+        else:
+            x -= x_min
         if self.mode == "event":
             if not np.all(np.isnan(self.__x_last)):
                 x_last_max = self.__x_last.max()
@@ -127,24 +132,26 @@ class CompassSensor(CompoundEye):
         :type t: np.ndarray
         :return:
         """
-        if isinstance(x, ChromaticitySkyModel):
-            sky_model = x  # type: ChromaticitySkyModel
+        if isinstance(x, SkyModel):
+            sky_model = x  # type: SkyModel
             x = np.empty((0, self.L.size), dtype=np.float32)
             t = np.empty((0, NB_EN), dtype=np.float32)
-            r = self.yaw_pitch_roll[0]
+            r = self.yaw
             for j in xrange(180):
                 self.rotate(np.deg2rad(2))
                 self.sky = sky_model
-                lon = (sky_model.lon + self.yaw_pitch_roll[0]) % (2 * np.pi)
+                lon = (sky_model.lon + self.yaw) % (2 * np.pi)
                 lat = sky_model.lat
                 x = np.vstack([x, self.L.flatten()])
                 t = np.vstack([t, encode_sun(lon, lat)])
-            self.yaw_pitch_roll[0] = r
+            self.yaw = r
             self.sky = sky_model
 
         # self.w = (1. - self.learning_rate) * self.w + self.learning_rate * la.pinv(x).dot(t)
-        self.w_whitening = pca_kernel(x)
+        x[np.isnan(x)] = 0.
         self.m = x.mean(axis=0)
+        if self.kernel is not None:
+            self.w_whitening = self.kernel(x)
         if t is not None:
             self.w = la.pinv(self._pprop(x), 1e-01).dot(t)
 
@@ -165,6 +172,7 @@ class CompassSensor(CompoundEye):
         return self._fprop(self.L, decode=decode).flatten()
 
     def _pprop(self, x):
+        # return x
         return (x.reshape((-1, self.nb_lenses)) - self.m).dot(self.w_whitening)
 
     def _fprop(self, x, decode=True):
@@ -190,7 +198,7 @@ class CompassSensor(CompoundEye):
         self.w_whitening = weights["w_whitening"]
 
     @classmethod
-    def visualise(cls, sensor):
+    def visualise(cls, sensor, interactive=False):
         """
 
         :param sensor:
@@ -199,6 +207,8 @@ class CompassSensor(CompoundEye):
         """
         import matplotlib.pyplot as plt
         from matplotlib.patches import Ellipse, Rectangle
+        if interactive:
+            plt.ion()
 
         xyz = sph2vec(sensor.theta_local, sensor.phi_local, sensor.R_c)
 
@@ -357,7 +367,11 @@ class CompassSensor(CompoundEye):
 
         plt.tight_layout(pad=0.)
 
-        plt.show()
+        if interactive:
+            plt.draw()
+            plt.pause(.1)
+        else:
+            plt.show()
 
 
 def encode_sun(lon, lat):
@@ -415,7 +429,7 @@ if __name__ == "__main__":
     from datetime import datetime
 
     # modes: "normal", "cross", "event"
-    s = CompassSensor(nb_lenses=1000, fov=np.deg2rad(60), mode="normal")
+    s = CompassSensor(nb_lenses=60, fov=np.deg2rad(60), mode="normal")
     # s.load_weights()
 
     # default observer is in Seville (where the data come from)
@@ -423,8 +437,7 @@ if __name__ == "__main__":
     observer.date = datetime(2018, 6, 21, 10, 0, 0)
 
     s.sky.obs = observer
-    for angle in np.linspace(0, np.pi/2, 9):
-        s.rotate(yaw=np.pi/6, roll=np.pi / 18)
+    for angle in np.linspace(0, 3 * np.pi, 9, endpoint=False):
         s.refresh()
         # lon, lat = sky.lon, sky.lat
         # print "Reality: Lon = %.2f, Lat = %.2f" % (np.rad2deg(lon), np.rad2deg(lat))
@@ -432,7 +445,8 @@ if __name__ == "__main__":
         # print "Prediction: Lon = %.2f, Lat = %.2f" % (np.rad2deg(lon), np.rad2deg(lat))
         # s.rotate(np.deg2rad(90))
         # s.set_sky(sky)
-        CompassSensor.visualise(s)
+        CompassSensor.visualise(s, interactive=True)
+        s.rotate(yaw=np.pi/3)
 
 
 if __name__ == "__main__2__":

compoundeye/test_sensor.py
@@ -3,48 +3,56 @@ from sensor import CompassSensor, NB_EN, decode_sun
 from learn import get_loss
 from sky import get_seville_observer, SkyModel
 from datetime import datetime, timedelta
+from learn.whitening import pca, zca
 import os
 
 
 __dir__ = os.path.dirname(os.path.realpath(__file__)) + "/"
 __datadir__ = __dir__ + "../data/datasets/"
 mse = get_loss("ad3")
+std = get_loss("astd3")
 
 
 if __name__ == "__main__":
     import matplotlib.pyplot as plt
 
     # parameters
-    nb_lenses = 60
-    fov = 60
+    nb_lenses = 368
+    fov = 180
+    kernel = zca
     observer = get_seville_observer()
     nb_months = 7
     start_month = 6
     start_day = 21
     delta = timedelta(hours=1)
-    mode = "monthly"
-    # mode = "hourly"
+    # mode = "monthly"
+    mode = "hourly"
     # mode = 6
 
     name = "seville-F%03d-I%03d-O%03d-M%02d-D%04d" % (fov, nb_lenses, NB_EN, nb_months, delta.seconds)
     data = np.load(__datadir__ + "%s.npz" % name)
     dates = np.load(__datadir__ + "M%02d-D%04d.npz" % (nb_months, delta.seconds))['m']
 
-    s = CompassSensor(nb_lenses=nb_lenses, fov=np.deg2rad(fov))
+    s = CompassSensor(nb_lenses=nb_lenses, fov=np.deg2rad(fov), kernel=kernel)
 
     # create and generate a sky instance
     observer.date = datetime.now()
-    sky = SkyModel(observer=observer, nside=1)
-    sky.generate()
+    s.sky.obs = observer
+    s.sky.generate()
 
     x, t = data['x'], data['t']
+    # x[np.isnan(x)] = 0.
     t = np.array([decode_sun(t0) for t0 in t])
-    y = s.update_parameters(x=data['x'], t=data['t'])
+    y = s.update_parameters(x=x, t=data['t'])
     s.save_weights()
 
     print "MSE:", mse(y, t)
     print "MSE-longitude:", mse(y, t, theta=False)
     print "MSE-latitude:", mse(y, t, phi=False)
+
+    print "STD:", std(y, t)
+    print "STD-longitude:", std(y, t, theta=False)
+    print "STD-latitude:", std(y, t, phi=False)
 
     S = 360
     step = 10
@@ -87,6 +95,8 @@ if __name__ == "__main__":
                 plt.title("%d:00" % dates[i][j*S][0].hour)
     elif isinstance(mode, basestring) and mode == "hourly":
         plt.figure("hourly-binned-mse", figsize=(15, 5))
+        ylim = 20
+
         mse_x, mse_y, mse_y_lon, mse_y_lat = [], [], [], []
         for h in xrange(24):
             i = np.array([True if date[0].month == 6 and date[0].hour == h else False for ii, date in enumerate(dates)])
@@ -102,7 +112,7 @@ if __name__ == "__main__":
         plt.plot(mse_x, mse_y_lat, label="MSE-lat")
         plt.legend()
         plt.xlim([5, 19])
-        plt.ylim([0, 22])
+        plt.ylim([0, ylim])
         plt.xlabel("Time (hour)")
         plt.ylabel("MSE")
         plt.title("June")
@@ -122,7 +132,7 @@ if __name__ == "__main__":
         plt.plot(mse_x, mse_y_lat, label="MSE-lat")
         plt.legend()
         plt.xlim([5, 19])
-        plt.ylim([0, 22])
+        plt.ylim([0, ylim])
         plt.xlabel("Time (hour)")
         plt.ylabel("MSE")
         plt.title("September")
@@ -142,17 +152,17 @@ if __name__ == "__main__":
         plt.plot(mse_x, mse_y_lat, label="MSE-lat")
         plt.legend()
         plt.xlim([5, 19])
-        plt.ylim([0, 22])
+        plt.ylim([0, ylim])
         plt.xlabel("Time (hour)")
         plt.ylabel("MSE")
         plt.title("December")
     plt.show()
 
-    lon, lat = sky.lon, sky.lat
+    s.rotate(-s.yaw, -s.pitch, -s.roll)
+    lon, lat = (s.sky.lon + np.pi) % (2 * np.pi) - np.pi, (s.sky.lat + np.pi) % (2 * np.pi) - np.pi
     print "Reality: Lon = %.2f, Lat = %.2f" % (np.rad2deg(lon), np.rad2deg(lat))
-    s.update_parameters(x=data['x'], t=data['t'])[0]
-    s.facing_direction = 0
-    lon, lat = s(sky)
+    lon, lat = s(s.sky, decode=True)
+    lon, lat = (lon + np.pi) % (2 * np.pi) - np.pi, (lat + np.pi) % (2 * np.pi) - np.pi
     print "Prediction: Lon = %.2f, Lat = %.2f" % (np.rad2deg(lon), np.rad2deg(lat))
     # CompassSensor.visualise(s)
 

learn/loss_function.py
@@ -33,6 +33,24 @@ def angular_distance_3d(y_predict, y_target, theta=True, phi=True):
     return np.rad2deg(np.arccos((v1 * v2).sum(axis=0)).mean())
 
 
+def angular_deviation_3d(y_predict, y_target, theta=True, phi=True):
+    if theta:
+        thy = y_predict[:, 1]
+        tht = y_target[:, 1]
+    else:
+        thy = np.zeros_like(y_predict[:, 1])
+        tht = np.zeros_like(y_target[:, 1])
+    if phi:
+        phy = y_predict[:, 0]
+        pht = y_target[:, 0]
+    else:
+        phy = np.zeros_like(y_predict[:, 0])
+        pht = np.zeros_like(y_target[:, 0])
+    v1 = sph2vec(thy, phy)
+    v2 = sph2vec(tht, pht)
+    return np.rad2deg(np.arccos((v1 * v2).sum(axis=0)).std())
+
+
 losses = {
     "adr": angular_distance_rad,
     "angular distance rad": angular_distance_rad,
@@ -41,7 +59,9 @@ losses = {
     "adp": angular_distance_per,
     "angular distance percentage": angular_distance_per,
     "ad3": angular_distance_3d,
-    "angular distance 3D": angular_distance_3d
+    "angular distance 3D": angular_distance_3d,
+    "astd3": angular_deviation_3d,
+    "angular deviation 3D": angular_deviation_3d
 }
 
 