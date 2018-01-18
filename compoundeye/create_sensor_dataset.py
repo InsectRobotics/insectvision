Fix bug in sensor - fix angle of polarisation rotation bug - fix the visualisation of the sensor - update sensor to work with tilted skies - change the computational model from linear to multilayer and non-linear - validate with sun position - update data-sets

compoundeye/create_sensor_dataset.py
@@ -11,6 +11,9 @@ __datadir__ = __dir__ + "../data/datasets/"
 if __name__ == "__main__":
 
     # parameters
+    mode = 'normal'
+    fibonacci = False
+    tilting = False
     nb_lenses = 368
     fov = 180
     observer = get_seville_observer()
@@ -24,9 +27,13 @@ if __name__ == "__main__":
     t = np.empty((0, NB_EN), dtype=np.float32)
     m = np.empty((0, 1), dtype='datetime64[s]')
     name = "seville-F%03d-I%03d-O%03d-M%02d-D%04d" % (fov, nb_lenses, NB_EN, nb_months, delta.seconds)
+    if tilting:
+        name += "-tilt"
+    if fibonacci:
+        name += "-fibonacci"
     print name
 
-    sensor = CompassSensor(nb_lenses=nb_lenses, fov=np.deg2rad(fov))
+    sensor = CompassSensor(nb_lenses=nb_lenses, fov=np.deg2rad(fov), mode=mode, fibonacci=fibonacci)
 
     months = (np.arange(start_month-1, start_month + nb_months - 1, 1) % 12) + 1
     for month in months:
@@ -39,29 +46,67 @@ if __name__ == "__main__":
         while sensor.sky.obs.date.datetime() < setting:
             sensor.sky.generate()
 
+            if tilting:
+                tilt = [-30, -15, 0, 15, 30]
+                # sensor.rotate(pitch=np.deg2rad(-30))
+            else:
+                tilt = [0]
+
             print "Date:", sensor.sky.obs.date,
-            for i in xrange(360):
-                sensor.refresh()
-                # lon, lat, _ = CompassSensor.rotate_centre(
-                #     np.array([sensor.sky.lon, sensor.sky.lat, 0]),
-                #     yaw=sensor.yaw,
-                #     pitch=sensor.pitch,
-                #     roll=sensor.roll
-                # )
-                lat = sensor.sky.lat
-                lon = (sensor.sky.lon - sensor.yaw + np.pi) % (2 * np.pi) - np.pi
-                x = np.vstack([x, sensor.L])
-                t = np.vstack([t, encode_sun(lon, lat)])
-                m = np.vstack([m, sensor.sky.obs.date.datetime()])
-                if i % 10 == 0:
-                    print '.',
-                    # print '  ' * (j+1) ** 2,
-                sensor.rotate(yaw=np.deg2rad(1))
+            for j in tilt:
+                sensor.rotate(pitch=np.deg2rad(j)-sensor.pitch)
+                for i in xrange(360):
+                    sensor.refresh()
+                    # lon, lat, _ = CompassSensor.rotate_centre(
+                    #     np.array([sensor.sky.lon, sensor.sky.lat, 0]),
+                    #     yaw=sensor.yaw,
+                    #     pitch=sensor.pitch,
+                    #     roll=sensor.roll
+                    # )
+                    lat = sensor.sky.lat
+                    lon = (sensor.sky.lon - sensor.yaw + np.pi) % (2 * np.pi) - np.pi
+                    x = np.vstack([x, sensor.L])
+                    t = np.vstack([t, encode_sun(lon, lat)])
+                    m = np.vstack([m, sensor.sky.obs.date.datetime()])
+                    if i % 50 == 0:
+                        print '.',
+                        # print '  ' * (j+1) ** 2,
+                    sensor.rotate(yaw=np.deg2rad(1))
+            sensor.rotate(pitch=-sensor.pitch)
+
+            if tilting:
+                for j in tilt:
+                    sensor.rotate(roll=np.deg2rad(j)-sensor.pitch)
+                    for i in xrange(360):
+                        sensor.refresh()
+                        # lon, lat, _ = CompassSensor.rotate_centre(
+                        #     np.array([sensor.sky.lon, sensor.sky.lat, 0]),
+                        #     yaw=sensor.yaw,
+                        #     pitch=sensor.pitch,
+                        #     roll=sensor.roll
+                        # )
+                        lat = sensor.sky.lat
+                        lon = (sensor.sky.lon - sensor.yaw + np.pi) % (2 * np.pi) - np.pi
+                        x = np.vstack([x, sensor.L])
+                        t = np.vstack([t, encode_sun(lon, lat)])
+                        m = np.vstack([m, sensor.sky.obs.date.datetime()])
+                        if i % 50 == 0:
+                            print '.',
+                            # print '  ' * (j+1) ** 2,
+                        sensor.rotate(yaw=np.deg2rad(1))
+                sensor.rotate(roll=-sensor.roll)
             sensor.refresh()
             print " ", x.shape, t.shape
-            # sensor.rotate(yaw=-sensor.yaw)
 
             sensor.sky.obs.date = sensor.sky.obs.date.datetime() + delta
 
-    np.savez_compressed(__datadir__ + "%s.npz" % name, x=x, t=t)
-    np.savez_compressed(__datadir__ + "M%02d-D%04d.npz" % (nb_months, delta.seconds), m=m)
+    if mode == 'normal':
+        mode = ''
+    else:
+        mode += '-'
+    np.savez_compressed(__datadir__ + "%s%s.npz" % (mode, name), x=x, t=t)
+    if tilting:
+        dates_name = __datadir__ + "%sM%02d-D%04d-tilted.npz"
+    else:
+        dates_name = __datadir__ + "%sM%02d-D%04d.npz"
+    np.savez_compressed(dates_name % (mode, nb_months, delta.seconds), m=m)

compoundeye/model.py
@@ -1,4 +1,5 @@
 import numpy as np
+from copy import copy
 from utils import get_microvilli_angle
 from sky import SkyModel
 
@@ -74,10 +75,11 @@ class CompoundEye(object):
         for kk in ['r', 'g', 'b', 'uv']:
             if kk in ks:
                 k.append(kk)
-        i, d, a = self.sky.L / np.sqrt(2), self.sky.DOP, self.sky.AOP
+        i, d, a = self.sky.L / np.sqrt(2), self.DOP, self.AOP
         lum_channels = np.array([
             pf(cf(i), a, d) for cf, pf in [self._channel_filters[c] for c in k]
         ]).T
+        lum_channels[np.isclose(i, 0.)] *= 0.
         return np.clip(lum_channels, 0, 1)
 
     @property
@@ -86,7 +88,7 @@ class CompoundEye(object):
 
     @property
     def AOP(self):
-        return self.sky.AOP
+        return (self.yaw + self.sky.AOP) % (2 * np.pi)
 
     @property
     def theta_global(self):
@@ -121,7 +123,7 @@ class CompoundEye(object):
         theta_z, phi_z = SkyModel.rotate(self.theta_global, self.phi_global, yaw=-self.yaw)
         theta_z, phi_z = SkyModel.rotate(theta_z, phi_z, pitch=-self.pitch)
         theta_z, phi_z = SkyModel.rotate(theta_z, phi_z, roll=-self.roll)
-        return (np.pi + phi_z) % (2 * np.pi) - np.pi
+        return (phi_z + np.pi) % (2 * np.pi) - np.pi
 
     @property
     def yaw(self):
@@ -157,7 +159,7 @@ class CompoundEye(object):
 
         # update the facing direction of the eye
         self.yaw_pitch_roll = self.rotate_centre(
-            self.yaw_pitch_roll, yaw=yaw, pitch=pitch, roll=roll
+            self.yaw_pitch_roll, yaw=yaw, pitch=-pitch, roll=roll
         )
 
         # rotate the sky according to the new facing direction
@@ -188,7 +190,7 @@ class CompoundEye(object):
     @staticmethod
     def rotate_centre(centre, yaw=0., pitch=0., roll=0.):
         # centre[[1, 0]] = SkyModel.rotate(centre[1], centre[0], yaw=yaw, pitch=pitch, roll=roll)
-        centre[[1, 0]] = SkyModel.rotate(np.pi / 2 - centre[1], np.pi - centre[0], yaw=yaw, pitch=pitch)
+        centre[[1, 0]] = SkyModel.rotate(np.pi / 2 - centre[1], np.pi - centre[0], yaw=yaw, pitch=-pitch, roll=-roll)
 
         centre[0] = (2 * np.pi - centre[0]) % (2 * np.pi) - np.pi
         centre[1] = (3 * np.pi/2 - centre[1]) % (2 * np.pi) - np.pi
@@ -228,7 +230,7 @@ class WLFilter(Filter):
         self.weight = self.wl2int(wl_max)
 
     def __call__(self, *args, **kwargs):
-        i0 = super(WLFilter, self).__call__(*args, **kwargs)
+        i0 = copy(super(WLFilter, self).__call__(*args, **kwargs))
         i0 += self.weight * (1. - i0)
         return np.clip(i0, 0., 1.)
 
@@ -260,16 +262,17 @@ class POLFilter(Filter):
             dop = kwargs['dop']
         # print self.name,
 
+        i0 = lum / (1. - dop)
         # create the light coordinates
         d = aop - self.angle
         E1 = np.array([
             np.cos(d),
             np.sin(d)
-        ]) * lum
+        ]) * np.sqrt(i0)
         E2 = np.array([
             np.cos(d + np.pi/2),
             np.sin(d + np.pi/2)
-        ]) * lum * (1. - dop)
+        ]) * np.sqrt(i0) * (1. - dop)
 
         # filter the separate coordinates
         E1[1] *= (1. - self.degree)
@@ -277,8 +280,9 @@ class POLFilter(Filter):
 
         # the total intensity is the integration of the ellipse
         E = np.array([np.sqrt(np.square(E1).sum(axis=0)), np.sqrt(np.square(E2).sum(axis=0))])
-        # print E[0].max(), E[1].max()
         return np.sqrt(np.square(E).sum(axis=0))
+        # A = np.sqrt(np.square(E1).sum(axis=0)) * np.sqrt(np.square(E2).sum(axis=0))
+        # return A
 
 
 if __name__ == "__main__":

compoundeye/sensor_parameters_analysis.py
@@ -9,8 +9,8 @@ import os
 MSE = get_loss("ad3")
 __dir__ = os.path.dirname(os.path.realpath(__file__)) + "/"
 __datadir__ = __dir__ + "../data/datasets/"
-nb_lenses = [4, 12, 60, 100, 220, 840]
-fovs = [(14, 4), (29, 12), (60, 60), (90, 112), (119, 176), (150, 272), (180, 368)]
+nb_lenses = [4, 12, 60, 112, 176, 368, 840]
+fovs = [(14, 4), (30, 12), (60, 60), (90, 112), (120, 176), (150, 272), (180, 368)]
 
 if __name__ == "__main__":
     import matplotlib.pyplot as plt
@@ -21,18 +21,20 @@ if __name__ == "__main__":
     start_month = 6
     start_day = 21
     delta = timedelta(hours=1)
-    # mode = "monthly"
-
-    plt.figure("MSE", figsize=(15, 10))
+    mode = "normal"
     fov_deg = 60
     fov = np.deg2rad(fov_deg)
+    generate_weights = True
+
+    plt.figure("MSE", figsize=(15, 10))
     mse, mse_lon, mse_lat = [], [], []
     for nb_lens in nb_lenses:
-        name = "seville-F%03d-I%03d-O%03d-M%02d-D%04d" % (fov_deg, nb_lens, NB_EN, nb_months, delta.seconds)
+        md = '' if mode == 'normal' else mode + '-'
+        name = "%sseville-F%03d-I%03d-O%03d-M%02d-D%04d" % (md, fov_deg, nb_lens, NB_EN, nb_months, delta.seconds)
         data = np.load(__datadir__ + "%s.npz" % name)
-        dates = np.load(__datadir__ + "M%02d-D%04d.npz" % (nb_months, delta.seconds))['m']
+        dates = np.load(__datadir__ + "%sM%02d-D%04d.npz" % (md, nb_months, delta.seconds))['m']
 
-        s = CompassSensor(nb_lenses=nb_lens, fov=np.deg2rad(fov_deg))
+        s = CompassSensor(nb_lenses=nb_lens, fov=np.deg2rad(fov_deg), mode=mode)
 
         # create and generate a sky instance
         observer.date = datetime.now()
@@ -41,7 +43,12 @@ if __name__ == "__main__":
 
         x, t = data['x'], data['t']
         t = np.array([decode_sun(t0) for t0 in t])
-        y = s.update_parameters(x=data['x'], t=data['t'])
+        if generate_weights:
+            y = s.update_parameters(x=data['x'], t=data['t'])
+            s.save_weights()
+        else:
+            s.load_weights()
+            y = s(data['x'], decode=True)
         mse.append(MSE(y, t))
         mse_lon.append(MSE(y, t, theta=False))
         mse_lat.append(MSE(y, t, phi=False))
@@ -56,18 +63,19 @@ if __name__ == "__main__":
     plt.title("Number of Lenses (FOV = 60 degrees)")
     plt.xticks(nb_lenses, nb_lenses)
     plt.xlim([0, 850])
-    plt.ylim([0, 35])
+    plt.ylim([0, 15])
     plt.ylabel("MSE (degrees)")
     plt.xlabel("Number of Lenses")
 
     # plt.figure("MSE - Field of view", figsize=(15, 10))
     mse, mse_lon, mse_lat, fovs_ = [],  [], [], []
     for fov_deg_, nb_lens_ in fovs:
-        name = "seville-F%03d-I%03d-O%03d-M%02d-D%04d" % (fov_deg_, nb_lens_, NB_EN, nb_months, delta.seconds)
+        md = '' if mode == 'normal' else mode + '-'
+        name = "%sseville-F%03d-I%03d-O%03d-M%02d-D%04d" % (md, fov_deg_, nb_lens_, NB_EN, nb_months, delta.seconds)
         data = np.load(__datadir__ + "%s.npz" % name)
-        dates = np.load(__datadir__ + "M%02d-D%04d.npz" % (nb_months, delta.seconds))['m']
+        dates = np.load(__datadir__ + "%sM%02d-D%04d.npz" % (md, nb_months, delta.seconds))['m']
 
-        s = CompassSensor(nb_lenses=nb_lens_, fov=np.deg2rad(fov_deg_))
+        s = CompassSensor(nb_lenses=nb_lens_, fov=np.deg2rad(fov_deg_), mode=mode)
 
         # create and generate a sky instance
         observer.date = datetime.now()
@@ -76,7 +84,12 @@ if __name__ == "__main__":
 
         x, t = data['x'], data['t']
         t = np.array([decode_sun(t0) for t0 in t])
-        y = s.update_parameters(x=data['x'], t=data['t'])
+        if generate_weights:
+            y = s.update_parameters(x=data['x'], t=data['t'])
+            s.save_weights()
+        else:
+            s.load_weights()
+            y = s(data['x'], decode=True)
         mse.append(MSE(y, t))
         mse_lon.append(MSE(y, t, theta=False))
         mse_lat.append(MSE(y, t, phi=False))
@@ -91,7 +104,7 @@ if __name__ == "__main__":
     plt.title("Field of View (similar density)")
     plt.xticks(fovs_)
     plt.xlim([0, 180])
-    plt.ylim([0, 35])
+    plt.ylim([0, 15])
     plt.xlabel("Field of View (degrees)")
 
     plt.show()

compoundeye/test_sensor.py
@@ -17,7 +17,10 @@ if __name__ == "__main__":
     import matplotlib.pyplot as plt
 
     # parameters
-    nb_lenses = 60
+    md = 'cross'
+    fibonacci = False
+    tilting = True
+    nb_lenses = 12
     fov = 60
     kernel = zca
     observer = get_seville_observer()
@@ -26,14 +29,22 @@ if __name__ == "__main__":
     start_day = 21
     delta = timedelta(hours=1)
     mode = "monthly"
+    # mode = "monthly-scatter"
     # mode = "hourly"
     # mode = 6
 
-    name = "seville-F%03d-I%03d-O%03d-M%02d-D%04d" % (fov, nb_lenses, NB_EN, nb_months, delta.seconds)
-    data = np.load(__datadir__ + "%s.npz" % name)
-    dates = np.load(__datadir__ + "M%02d-D%04d.npz" % (nb_months, delta.seconds))['m']
+    s = CompassSensor(nb_lenses=nb_lenses, fov=np.deg2rad(fov), kernel=kernel, mode=md, fibonacci=fibonacci)
 
-    s = CompassSensor(nb_lenses=nb_lenses, fov=np.deg2rad(fov), kernel=kernel)
+    md = '' if md == 'normal' else md + '-'
+    name = "%sseville-F%03d-I%03d-O%03d-M%02d-D%04d" % (md, fov, nb_lenses, NB_EN, nb_months, delta.seconds)
+    if tilting:
+        name += "-tilt"
+        dates = np.load(__datadir__ + "%sM%02d-D%04d-tilted.npz" % (md, nb_months, delta.seconds))['m']
+    else:
+        dates = np.load(__datadir__ + "%sM%02d-D%04d.npz" % (md, nb_months, delta.seconds))['m']
+    if fibonacci:
+        name += "-fibonacci"
+    data = np.load(__datadir__ + "%s.npz" % name)
 
     # create and generate a sky instance
     observer.date = datetime.now()
@@ -45,22 +56,28 @@ if __name__ == "__main__":
     t = np.array([decode_sun(t0) for t0 in t])
     y = s.update_parameters(x=x, t=data['t'])
     s.save_weights()
+    # s.load_weights()
+    # y = s(x, decode=True)
 
     print "MSE:", mse(y, t)
     print "MSE-longitude:", mse(y, t, theta=False)
     print "MSE-latitude:", mse(y, t, phi=False)
 
-    print "STD:", std(y, t)
-    print "STD-longitude:", std(y, t, theta=False)
-    print "STD-latitude:", std(y, t, phi=False)
+    print "SE:", std(y, t) / np.sqrt(y.shape[0])
+    print "SE-longitude:", std(y, t, theta=False) / np.sqrt(y.shape[0])
+    print "SE-latitude:", std(y, t, phi=False) / np.sqrt(y.shape[0])
 
     S = 360
     step = 10
 
-    if isinstance(mode, basestring) and mode == "monthly":
-        plt.figure("Monthly", figsize=(15, 10))
+    if isinstance(mode, basestring) and mode == "monthly-scatter":
+        import calendar
+
+        plt.figure("Monthly-12pm", figsize=(15, 10))
         for j in xrange(12):
-            i = np.array([True if date[0].month == j+1 and 8 <= date[0].hour <= 10 else False for ii, date in enumerate(dates)])
+            i = np.array([True if date[0].month == j+1 and 11 <= date[0].hour <= 13 else False for ii, date in enumerate(dates)])
+            if i.sum() == 0:
+                i = np.array([True if date[0].month == 11-j and 11 <= date[0].hour <= 13 else False for ii, date in enumerate(dates)])
             ax = plt.subplot(3, 4, j + 1, polar=True)
             ax.set_theta_zero_location("N")
             plt.scatter(
@@ -71,9 +88,73 @@ if __name__ == "__main__":
                 np.rad2deg(t[i][:S:step, 1]), label="target")
             for y0, t0 in zip(y[i][:S:step], t[i][:S:step]):
                 plt.plot([y0[0], t0[0]], [np.rad2deg(y0[1]), np.rad2deg(t0[1])], 'k-')
+            plt.text(0, 0, calendar.month_name[j+1], fontsize=16,
+                     bbox=dict(facecolor='white', alpha=.5),
+                     horizontalalignment='center', verticalalignment='center')
             plt.xlim([-180, 180])
             plt.ylim([0, 90])
             # plt.legend()
+    elif isinstance(mode, basestring) and mode == "monthly":
+        plt.figure("Monthly", figsize=(15, 10))
+        mse_x, mse_y, mse_y_lon, mse_y_lat = [], [], [], []
+        std_x, std_y, std_y_lon, std_y_lat = [], [], [], []
+        ylim = 20
+
+        for j in xrange(12):
+            i = np.array([True if date[0].month == j+1 else False for ii, date in enumerate(dates)])
+            n = i.sum()
+            if n == 0:
+                continue
+            mse_x.append(j+1)
+            std_x.append(j+1)
+            mse_y.append(mse(y[i], t[i]))
+            mse_y_lon.append(mse(y[i], t[i], theta=False))
+            mse_y_lat.append(mse(y[i], t[i], phi=False))
+            std_y.append(std(y[i], t[i]) / np.sqrt(n))
+            std_y_lon.append(std(y[i], t[i], theta=False) / np.sqrt(n))
+            std_y_lat.append(std(y[i], t[i], phi=False) / np.sqrt(n))
+
+        for j in xrange(12):
+            if j+1 not in mse_x:
+                mse_y.append(mse_y[mse_x.index(11 - j)])
+                mse_y_lon.append(mse_y_lon[mse_x.index(11 - j)])
+                mse_y_lat.append(mse_y_lat[mse_x.index(11 - j)])
+                mse_x.append(j+1)
+            if j+1 not in std_x:
+                std_y.append(std_y[std_x.index(11 - j)])
+                std_y_lon.append(std_y_lon[std_x.index(11 - j)])
+                std_y_lat.append(std_y_lat[std_x.index(11 - j)])
+                std_x.append(j+1)
+
+        i = np.argsort(mse_x)
+        mse_x = np.array(mse_x)[i]
+        mse_y = np.array(mse_y)[i]
+        mse_y_lon = np.array(mse_y_lon)[i]
+        mse_y_lat = np.array(mse_y_lat)[i]
+        i = np.argsort(std_x)
+        std_x = np.array(std_x)[i]
+        std_y = np.array(std_y)[i]
+        std_y_lon = np.array(std_y_lon)[i]
+        std_y_lat = np.array(std_y_lat)[i]
+        scale = 3.
+
+        # plt.subplot(131)
+        plt.fill_between(std_x, mse_y - scale * std_y, mse_y + scale * std_y,
+                         facecolor="#3F5D7D", alpha=.5)
+        plt.plot(mse_x, mse_y, label="MSE")
+        plt.fill_between(std_x, mse_y_lon - scale * std_y_lon, mse_y_lon + scale * std_y_lon,
+                         facecolor="#7D3F5D", alpha=.5)
+        plt.plot(mse_x, mse_y_lon, label="MSE-lon")
+        plt.fill_between(std_x, mse_y_lat - scale * std_y_lat, mse_y_lat + scale * std_y_lat,
+                         facecolor="#3F7D5D", alpha=.5)
+        plt.plot(mse_x, mse_y_lat, label="MSE-lat")
+        plt.legend()
+        plt.xlim([1, 12])
+        plt.xticks(np.arange(1, 12))
+        plt.ylim([0, ylim])
+        plt.xlabel("Time (month)")
+        plt.ylabel("MSE (degrees)")
+        plt.title("Monthly")
     elif isinstance(mode, int):
         plt.figure(datetime(year=2017, month=mode, day=1).strftime("%B %Y"), figsize=(15, 10))
         i = np.array([True if date[0].month == mode else False for ii, date in enumerate(dates)])
@@ -91,11 +172,14 @@ if __name__ == "__main__":
                 plt.plot([y0[0], t0[0]], [np.rad2deg(y0[1]), np.rad2deg(t0[1])], 'k-')
             plt.xlim([-180, 180])
             plt.ylim([0, 90])
-            if j < 4:
-                plt.title("%d:00" % dates[i][j*S][0].hour)
+            k = ((j + 2) / 6) % 2
+            plt.text(k * np.pi, k * 60, "%d:00" % dates[i][j*S][0].hour,
+                     bbox=dict(facecolor='white', alpha=.5), fontsize=16,
+                     horizontalalignment='center', verticalalignment='center')
+            # plt.title("%d:00" % dates[i][j*S][0].hour)
     elif isinstance(mode, basestring) and mode == "hourly":
         plt.figure("hourly-binned-mse", figsize=(15, 5))
-        ylim = 20
+        ylim = 50
 
         mse_x, mse_y, mse_y_lon, mse_y_lat = [], [], [], []
         for h in xrange(24):
@@ -161,7 +245,7 @@ if __name__ == "__main__":
     s.rotate(-s.yaw, -s.pitch, -s.roll)
     lon, lat = (s.sky.lon + np.pi) % (2 * np.pi) - np.pi, (s.sky.lat + np.pi) % (2 * np.pi) - np.pi
     print "Reality: Lon = %.2f, Lat = %.2f" % (np.rad2deg(lon), np.rad2deg(lat))
-    lon, lat = s(s.sky, decode=True)
+    lon, lat = s(s.sky, decode=True).flatten()
     lon, lat = (lon + np.pi) % (2 * np.pi) - np.pi, (lat + np.pi) % (2 * np.pi) - np.pi
     print "Prediction: Lon = %.2f, Lat = %.2f" % (np.rad2deg(lon), np.rad2deg(lat))
     # CompassSensor.visualise(s)